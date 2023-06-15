d3e67aa8c52389ee8bb95cc2d7126cf5e0483d06
dennoh-mzoefu <denisgichana162@gmail.com>
2023-06-15 09:20:27 +0300

    mitigate mass assignment vulnerabilities

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+    
+    // protected $fillable = ['title','excerpt','body','id']; // declares fields that are fillable with create or update methods
+    // protected $guarded = [];  //declares the attributes that arent fillable inthis case all are not fillable
 }
